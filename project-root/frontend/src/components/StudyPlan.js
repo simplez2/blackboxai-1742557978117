5774f37c051321752df9ed3b28c730992101801c
zhenyu lin <zhen3363494168@gmail.com>
2025-03-21 19:53:29 +0800

    Add project-root/frontend/src/components/StudyPlan.js 1742558007614

---
+++ project-root/frontend/src/components/StudyPlan.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StudyPlan = () => {
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold">Study Plan</h1>
+            {/* Placeholder for the study plan visualization */}
+            <p>Your study plan will be displayed here.</p>
+        </div>
+    );
+};
+
+export default StudyPlan;
